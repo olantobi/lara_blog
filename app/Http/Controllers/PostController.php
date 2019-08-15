5542951ba069c30511a24fb840cc62eb91f02422
Tobi Olanrewaju <olantobi@gmail.com>
2019-08-15 05:43:34 +0100

    Application with endpoints ready

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+        return Post::all();
+    }
+
+    public function show (Post $post) {
+        return $post;
+    }
+
+    public function store(Request $request) {
+        $post = Post::create($request->all());
+        return response()->json($post, 201);
+    }
+
+    public function update(Request $request, Post $post) {
+        $post->update($request->all());
+        return response()->json($post);
+    }
+
+    public function delete(Post $post) {
+        $post->delete();
+        return response()->json(null, 204);
+    }
+}
